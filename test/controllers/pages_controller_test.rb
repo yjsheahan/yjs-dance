e44a0fc6a74e7b43985c014da01e865a529e9e7f
Yasmin Jones-Sheahan <y.jsheahan@gmail.com>
2020-06-28 13:13:33 +0100

    routes for pages simplified and boostrap, jquery and popper.js installed

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get portfolio" do
+    get pages_portfolio_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get pages_contact_url
+    assert_response :success
+  end
+
+end
