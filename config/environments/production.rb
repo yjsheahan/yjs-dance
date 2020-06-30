29a2094893aba48a8eb4459f945d69c22f443c5e
Yasmin Jones-Sheahan <y.jsheahan@gmail.com>
2020-06-30 11:45:22 +0100

    missed an end to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,18 +122,4 @@ Rails.application.configure do
   :authentication => :plain,
   :enable_starttls_auto => true
   }
-
-#   config.action_mailer.default_url_options = { host: 'https://yasminjs-dance.herokuapp.com' }
-#   config.action_mailer.delivery_method = :smtp
-
-
-#   ActionMailer::Base.smtp_settings = {
-#   :user_name => ENV['SENDGRID_USERNAME'],
-#   :password => ENV['SENDGRID_PASSWORD'],
-#   :domain => 'heroku.com',
-#   :address => 'smtp.sendgrid.net',
-#   :port => 587,
-#   :authentication => :plain,
-#   :enable_starttls_auto => true
-#   }
-# end
+end
